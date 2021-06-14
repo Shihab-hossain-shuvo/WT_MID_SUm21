0f0c180d71eeb4c44e97a89fb79f355f55851c74
shihab <shuvokhanrs2015@gmail.com>
2021-06-14 09:53:54 +0600

    lab task3

---
+++ lab3/problem1.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Problem 1</title>
+	</head>
+	<body>
+	
+		<?php
+			$var1 = "fox";
+			$var2 = "dog";
+			echo "A quick brown <b>".$var1 . " </b>jumps over the lazy <b>".$var2 . "</b>";
+			
+			
+		?>
+
+	</body>
+</html>
---
+++ lab3/problem2.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>problem2</title>
+	</head>
+	<body>
+	
+		<?php
+			$mark = 90;
+			if ($mark >=90){
+				echo "A+";
+			}elseif ($mark >= 80){
+				echo "B";
+			}elseif ($mark >= 70)
+				echo "C";
+				
+			else{
+				echo "F";
+			}	
+			
+		?>
+
+	</body>
+</html>
